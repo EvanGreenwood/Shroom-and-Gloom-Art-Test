465fe57cbd4293acfd0a27b7c5604364bbe51b67
mathbad <overmarvelous@gmail.com>
2024-05-06 22:24:11 +0200

    mine stuff

---
+++ Assets/Scripts/CameraValidator.cs
@@ -0,0 +1,12 @@
+#region Usings
+using UnityEngine;
+using Framework;
+#endregion
+
+public class CameraValidator : MonoBehaviour
+{
+    // MonoBehaviour
+    //----------------------------------------------------------------------------------------------------
+    void Awake() {}
+    void Update() {}
+}
---
+++ Assets/Scripts/Utility/RandomSprite.cs
@@ -0,0 +1,12 @@
+#region Usings
+using UnityEngine;
+using Framework;
+#endregion
+
+public class RandomSprite : MonoBehaviour
+{
+    // MonoBehaviour
+    //----------------------------------------------------------------------------------------------------
+    void Awake() {}
+    void Update() {}
+}
