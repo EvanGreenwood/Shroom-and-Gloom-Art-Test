beaf7f20b35ee49dc657da4a5ae221c291a988da
mathbad <overmarvelous@gmail.com>
2024-05-07 12:13:26 +0200

    Mine stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+#region Usings
+using UnityEngine;
+using Framework;
+using Mainframe;
+#endregion
+
+public class GameController : SingletonBehaviour<GameController>
+{
+  void Update()
+  {
+    // if(INPUT.tab.pressed) INPUT.mouseCaptured = !INPUT.mouseCaptured;
+  }
+}
