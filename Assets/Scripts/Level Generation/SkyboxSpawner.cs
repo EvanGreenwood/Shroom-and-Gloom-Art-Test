dbf3cd705a9afe7b4e4a9e0c247f0dda24930b6f
mathbad <overmarvelous@gmail.com>
2024-05-10 10:16:35 +0200

    more cavern, hatching shader

--- Assets/Scripts/Level Generation/SkyboxSpawner.cs
+++ Assets/Scripts/Level Generation/SkyboxSpawner.cs
@@ -8,7 +8,8 @@ public class SkyboxSpawner : MonoBehaviour
 {
     [SerializeField] GameObject[] _prefabs;
     [SerializeField] int _spawnCount = 100;
-    [SerializeField] FloatRange _scaleFactor = new FloatRange(1f, 2f);
+    [SerializeField] FloatRange _scaleFactorX = new FloatRange(1f, 2f);
+    [SerializeField] FloatRange _scaleFactorY = new FloatRange(1f, 2f);
     [SerializeField] Bounds _spawnArea;
 
     void Awake()
@@ -17,7 +18,8 @@ public class SkyboxSpawner : MonoBehaviour
         {
             Vector3 spawnPos = RNG.Vector3(_spawnArea.min, _spawnArea.max);
             GameObject spawned = Instantiate(_prefabs.ChooseRandom(), spawnPos, Quaternion.identity);
-            spawned.transform.localScale *= _scaleFactor.ChooseRandom();
+            Vector3 factor = new Vector3(_scaleFactorX.ChooseRandom(), _scaleFactorY.ChooseRandom(), 1f);
+            spawned.transform.localScale = Vector3.Scale(spawned.transform.localScale, factor);
         }
     }
     void OnDrawGizmos()
